febf3301497928c65eb369779ff25ad2b646636e
Mike pH Rasmussen <asusvenus@gmail.com>
2018-12-04 19:21:03 +0100

    added blog comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
-#Rails.application.routes.draw do
-#  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#  resources :articles
-#end
-
+##Rails.application.routes.draw do
+###  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+##  resources :articles :comments
+##end
+#
 Blogger::Application.routes.draw do
   root to: 'articles#index'
-  resources :articles
-end
+  resources :articles do
 
+end
+resources :articles do
+    resources :comments
+  end
+end
+  
+
+  
+
+  
